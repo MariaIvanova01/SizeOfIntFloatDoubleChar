0f03dbfc955568d0d7db59e7ae0b24f4d2b9c427
MariaIvanova01 <mimi200801@abv.bg>
2021-03-25 18:20:13 +0200

    Counting the size of int, float, double and char

---
+++ size of int float double char/size of int float double char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    cout << "intType = " << sizeof(intType) << endl;
+    cout << "floatType = " << sizeof(floatType) << endl;
+    cout << "doubleType = " << sizeof(doubleType) << endl;
+    cout << "charType = " << sizeof(charType) << endl;
+}
+
